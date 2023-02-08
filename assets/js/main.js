64148e1b3ea049ab6b7637d9dc6213f7a65a8cd5
Chloe <chloepap4@gmail.com>
2023-02-08 14:34:04 +0100

    fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,37 +10,45 @@
   /**
    * Easy selector helper function
    */
-  const select = (el, all = false) => {
-    el = el.trim()
-    if (all) {
-      return [...document.querySelectorAll(el)]
-    } else {
-      return document.querySelector(el)
-    }
-  }
+   const select = (el, all = false) => {
+     if (!el) {
+       return null
+     }
+     el = el.trim()
+     if (all) {
+       return [...document.querySelectorAll(el)]
+     } else {
+       return document.querySelector(el)
+     }
+   }
 
   /**
    * Easy event listener function
-   */
-  const on = (type, el, listener, all = false) => {
+   */  const on = (type, el, listener, all = false) => {
     let selectEl = select(el, all)
     if (selectEl) {
       if (all) {
-        selectEl.forEach(e => e.addEventListener(type, listener))
+        selectEl.forEach(e => {
+          if (e !== null) {
+            e.addEventListener(type, listener)
+          }
+        })
       } else {
-        selectEl.addEventListener(type, listener)
+        if (selectEl !== null) {
+          selectEl.addEventListener(type, listener)
+        }
       }
     }
   }
 
   /**
-   * Easy on scroll event listener 
+   * Easy on scroll event listener
    */
   const onscroll = (el, listener) => {
     el.addEventListener('scroll', listener)
   }
 
-  
+
   /* Navbar links active state on scroll*/
   let navbarlinks = select('#navbar .scrollto', true)
   const navbarlinksActive = () => {
@@ -157,17 +165,17 @@
     }
   });
 
-  
+
   /**
    * Animation on scroll
    */
-  window.addEventListener('load', () => {
-    AOS.init({
-      duration: 1000,
-      easing: 'ease-in-out',
-      once: true,
-      mirror: false
-    })
-  });
+  // window.addEventListener('load', () => {
+  //   AOS.init({
+  //     duration: 1000,
+  //     easing: 'ease-in-out',
+  //     once: true,
+  //     mirror: false
+  //   })
+  // });
 
 })()
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,9 @@
 
 
 $(document).ready(function(){
+
+
+
   var start_style = sessionStorage.getItem('href');
   switch (start_style) {
     case 'assets/css/screenStyle.css':
@@ -25,7 +28,67 @@ $(document).ready(function(){
     case 'assets/css/future.css':
         document.getElementById('theme').setAttribute('href', 'assets/css/future.css');
         break;}
+
+
+
+
   });
+
+
+
+
+/* change themes*/
+function changeTheme(newTheme) {
+  const elem = document.getElementById('theme');
+  elem.href = newTheme;
+  sessionStorage.setItem('href', newTheme);
+  console.log(newTheme);
+}
+
+
+$(document).ajaxComplete(function(){
+
+  // add/remove classes for layout in issues
+  // Get the elements to apply the classes
+  // var article = document.querySelector('#canvas')
+  var titleDiv = document.querySelector('#style-title');
+  var innerDiv = document.querySelector('#row-style-title');
+  var innerCol = document.querySelector('#col-style-title');
+  var innerColAuthor = document.querySelector('#col-style-author');
+
+  // add the classes when the themed CSS file is applied
+  function addClasses() {
+    console.log(titleDiv);
+    if (titleDiv !== null && innerDiv !== null && innerCol !== null && innerColAuthor !== null) {
+
+    titleDiv.classList.add('style-title', 'flex', 'container', 'align-items-center', 'justify-content-center');
+    innerDiv.classList.add('style-title', 'flex', 'row', 'justify-content-center');
+    innerCol.classList.add('style-title', 'col', 'col-md-6', 'col-lg-6', 'col-6');
+    innerColAuthor.classList.add('style-title', 'flex', 'col', 'col-md-1', 'col-lg-1', 'col-1');
+  }
+}
+  //remove the classes when theme file is not applied
+  function removeClasses() {
+    if (titleDiv !== null && innerDiv !== null && innerCol !== null && innerColAuthor !== null) {
+
+
+    titleDiv.classList.remove('style-title', 'flex', 'container', 'align-items-center', 'justify-content-center');
+    innerDiv.classList.remove('style-title', 'flex', 'row', 'justify-content-center');
+    innerCol.classList.remove('style-title', 'col', 'col-md-6', 'col-lg-6', 'col-6');
+    innerColAuthor.classList.remove('style-title', 'flex', 'col', 'col-md-1', 'col-lg-1', 'col-1');
+
+  }
+}
+//
+// if (sessionStorage.getItem('href') === 'assets/css/bauhaus.css') {
+//   addClasses();
+// } else {
+//   removeClasses();
+// }
+
+// console.log(newTheme);    // rest of the code ...
+});
+
 
 
 function openNav() {
@@ -44,6 +107,11 @@ function closeNav() {
     document.getElementById("toggler").style.marginLeft = "0";
   }
 
+
+//
+// $(document).ready(function(){
+//
+// });
 
 
 // Get the modal
@@ -74,15 +142,4 @@ if (modal !== null && btn !== null && span !== null) {
       modal.style.display = "none";
     }
   }
-}
-
-
-
-
-
-/* change themes*/
-function changeTheme(newTheme) {
-  const elem = document.getElementById('theme');
-  elem.href = newTheme;
-  sessionStorage.setItem('href', newTheme);
 }
