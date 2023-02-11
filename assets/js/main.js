9a285244e9fb7766fb3fd8882001d9e450f1256f
Chloe <chloepap4@gmail.com>
2023-02-11 21:50:08 +0100

    fixing some issues

--- assets/js/main.js
+++ assets/js/main.js
@@ -161,87 +161,81 @@
 
   /**
    * Porfolio isotope and filter
-   */
-  window.addEventListener('load', () => {
-    let portfolioContainer = select('.portfolio-container');
-    if (portfolioContainer) {
-      let portfolioIsotope = new Isotope(portfolioContainer, {
-        itemSelector: '.portfolio-item',
-        layoutMode: 'fitRows'
-      });
-
-      let portfolioFilters = select('#portfolio-flters li', true);
-
-      on('click', '#portfolio-flters li', function(e) {
-        e.preventDefault();
-        portfolioFilters.forEach(function(el) {
-          el.classList.remove('filter-active');
-        });
-        this.classList.add('filter-active');
-
-        portfolioIsotope.arrange({
-          filter: this.getAttribute('data-filter')
-        });
-        portfolioIsotope.on('arrangeComplete', function() {
-          AOS.refresh()
-        });
-      }, true);
-    }
-
-  });
-
-  /**
-   * Initiate portfolio lightbox
-   */
-  const portfolioLightbox = GLightbox({
-    selector: '.portfolio-lightbox'
-  });
-
-  /**
-   * Portfolio details slider
-   */
-  new Swiper('.portfolio-details-slider', {
-    speed: 400,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    }
-  });
-
-  /**
-   * Testimonials slider
-   */
-  new Swiper('.testimonials-slider', {
-    speed: 600,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    slidesPerView: 'auto',
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    }
-  });
-
-  /**
-   * Animation on scroll
-   */
-  window.addEventListener('load', () => {
-    AOS.init({
-      duration: 1000,
-      easing: 'ease-in-out',
-      once: true,
-      mirror: false
-    })
-  });
-
+//    */
+//   window.addEventListener('load', () => {
+//     let portfolioContainer = select('.portfolio-container');
+//     if (portfolioContainer) {
+//       let portfolioIsotope = new Isotope(portfolioContainer, {
+//         itemSelector: '.portfolio-item',
+//         layoutMode: 'fitRows'
+//       });
+//
+//       let portfolioFilters = select('#portfolio-flters li', true);
+//
+//       on('click', '#portfolio-flters li', function(e) {
+//         e.preventDefault();
+//         portfolioFilters.forEach(function(el) {
+//           el.classList.remove('filter-active');
+//         });
+//         this.classList.add('filter-active');
+//
+//         portfolioIsotope.arrange({
+//           filter: this.getAttribute('data-filter')
+//         });
+//         portfolioIsotope.on('arrangeComplete', function() {
+//           AOS.refresh()
+//         });
+//       }, true);
+//     }
+//
+//   });
+//
+//
+//   /**
+//    * Portfolio details slider
+//    */
+//   new Swiper('.portfolio-details-slider', {
+//     speed: 400,
+//     loop: true,
+//     autoplay: {
+//       delay: 5000,
+//       disableOnInteraction: false
+//     },
+//     pagination: {
+//       el: '.swiper-pagination',
+//       type: 'bullets',
+//       clickable: true
+//     }
+//   });
+//
+//   /**
+//    * Testimonials slider
+//    */
+//   new Swiper('.testimonials-slider', {
+//     speed: 600,
+//     loop: true,
+//     autoplay: {
+//       delay: 5000,
+//       disableOnInteraction: false
+//     },
+//     slidesPerView: 'auto',
+//     pagination: {
+//       el: '.swiper-pagination',
+//       type: 'bullets',
+//       clickable: true
+//     }
+//   });
+//
+//   /**
+//    * Animation on scroll
+//    */
+//   window.addEventListener('load', () => {
+//     AOS.init({
+//       duration: 1000,
+//       easing: 'ease-in-out',
+//       once: true,
+//       mirror: false
+//     })
+//   });
+//
 })()
--- assets/js/script.js
+++ assets/js/script.js
@@ -30,7 +30,17 @@ $(document).ready(function(){
         break;}
 
 
+// separate function for issues navbar
+        $('#navbarTogglerDemo03 a').click(function () {
+          $('#navbarTogglerDemo03 a').removeClass('active');
+          $(this).addClass('active');
+        });
 
+// separate function for issues navbar collapse/toggle
+
+        $('.navbar-toggler').click(function() {
+          $('.navbar-collapse').toggle();
+        });
 
   });
 
@@ -45,49 +55,49 @@ function changeTheme(newTheme) {
   console.log(newTheme);
 }
 
-
-$(document).ajaxComplete(function(){
-
-  // add/remove classes for layout in issues
-  // Get the elements to apply the classes
-  // var article = document.querySelector('#canvas')
-  var titleDiv = document.querySelector('#style-title');
-  var innerDiv = document.querySelector('#row-style-title');
-  var innerCol = document.querySelector('#col-style-title');
-  var innerColAuthor = document.querySelector('#col-style-author');
-
-  // add the classes when the themed CSS file is applied
-  function addClasses() {
-    console.log(titleDiv);
-    if (titleDiv !== null && innerDiv !== null && innerCol !== null && innerColAuthor !== null) {
-
-    titleDiv.classList.add('style-title', 'flex', 'container', 'align-items-center', 'justify-content-center');
-    innerDiv.classList.add('style-title', 'flex', 'row', 'justify-content-center');
-    innerCol.classList.add('style-title', 'col', 'col-md-6', 'col-lg-6', 'col-6');
-    innerColAuthor.classList.add('style-title', 'flex', 'col', 'col-md-1', 'col-lg-1', 'col-1');
-  }
-}
-  //remove the classes when theme file is not applied
-  function removeClasses() {
-    if (titleDiv !== null && innerDiv !== null && innerCol !== null && innerColAuthor !== null) {
-
-
-    titleDiv.classList.remove('style-title', 'flex', 'container', 'align-items-center', 'justify-content-center');
-    innerDiv.classList.remove('style-title', 'flex', 'row', 'justify-content-center');
-    innerCol.classList.remove('style-title', 'col', 'col-md-6', 'col-lg-6', 'col-6');
-    innerColAuthor.classList.remove('style-title', 'flex', 'col', 'col-md-1', 'col-lg-1', 'col-1');
-
-  }
-}
 //
-// if (sessionStorage.getItem('href') === 'assets/css/bauhaus.css') {
-//   addClasses();
-// } else {
-//   removeClasses();
+// $(document).ajaxComplete(function(){
+//
+//   // add/remove classes for layout in issues
+//   // Get the elements to apply the classes
+//   // var article = document.querySelector('#canvas')
+//   var titleDiv = document.querySelector('#style-title');
+//   var innerDiv = document.querySelector('#row-style-title');
+//   var innerCol = document.querySelector('#col-style-title');
+//   var innerColAuthor = document.querySelector('#col-style-author');
+//
+//   // add the classes when the themed CSS file is applied
+//   function addClasses() {
+//     console.log(titleDiv);
+//     if (titleDiv !== null && innerDiv !== null && innerCol !== null && innerColAuthor !== null) {
+//
+//     titleDiv.classList.add('style-title', 'flex', 'container', 'align-items-center', 'justify-content-center');
+//     innerDiv.classList.add('style-title', 'flex', 'row', 'justify-content-center');
+//     innerCol.classList.add('style-title', 'col', 'col-md-6', 'col-lg-6', 'col-6');
+//     innerColAuthor.classList.add('style-title', 'flex', 'col', 'col-md-1', 'col-lg-1', 'col-1');
+//   }
 // }
-
-// console.log(newTheme);    // rest of the code ...
-});
+//   //remove the classes when theme file is not applied
+//   function removeClasses() {
+//     if (titleDiv !== null && innerDiv !== null && innerCol !== null && innerColAuthor !== null) {
+//
+//
+//     titleDiv.classList.remove('style-title', 'flex', 'container', 'align-items-center', 'justify-content-center');
+//     innerDiv.classList.remove('style-title', 'flex', 'row', 'justify-content-center');
+//     innerCol.classList.remove('style-title', 'col', 'col-md-6', 'col-lg-6', 'col-6');
+//     innerColAuthor.classList.remove('style-title', 'flex', 'col', 'col-md-1', 'col-lg-1', 'col-1');
+//
+//   }
+// }
+// //
+// // if (sessionStorage.getItem('href') === 'assets/css/bauhaus.css') {
+// //   addClasses();
+// // } else {
+// //   removeClasses();
+// // }
+//
+// // console.log(newTheme);    // rest of the code ...
+// });
 
 
 
