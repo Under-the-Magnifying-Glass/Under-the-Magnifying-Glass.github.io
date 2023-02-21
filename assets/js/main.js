0d17fa41389c5ed30da9c992cfa4c16c1c7975c8
Chloe <chloepap4@gmail.com>
2023-02-21 15:23:38 +0100

    fixed main.js errors HOPEFULLY

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,34 +5,44 @@
 * License: https://bootstrapmade.com/license/
 */
 
+/**
+ * Easy selector helper function
+ */
 (function() {
   "use strict";
 
-  /**
-   * Easy selector helper function
-   */
-  const select = (el, all = false) => {
-    el = el.trim()
-    if (all) {
-      return [...document.querySelectorAll(el)]
-    } else {
-      return document.querySelector(el)
-    }
-  }
+
+   const select = (el, all = false) => {
+     if (!el) {
+       return null
+     }
+     el = el.trim()
+     if (all) {
+       return [...document.querySelectorAll(el)]
+     } else {
+       return document.querySelector(el)
+     }
+   }
 
   /**
    * Easy event listener function
-   */
-  const on = (type, el, listener, all = false) => {
+   */  const on = (type, el, listener, all = false) => {
     let selectEl = select(el, all)
     if (selectEl) {
       if (all) {
-        selectEl.forEach(e => e.addEventListener(type, listener))
+        selectEl.forEach(e => {
+          if (e !== null) {
+            e.addEventListener(type, listener)
+          }
+        })
       } else {
-        selectEl.addEventListener(type, listener)
+        if (selectEl !== null) {
+          selectEl.addEventListener(type, listener)
+        }
       }
     }
   }
+
 
   /**
    * Easy on scroll event listener
--- assets/js/script.js
+++ assets/js/script.js
@@ -52,52 +52,7 @@ function changeTheme(newTheme) {
   const elem = document.getElementById('theme');
   elem.href = newTheme;
   sessionStorage.setItem('href', newTheme);
-  console.log(newTheme);
 }
-
-//
-// $(document).ajaxComplete(function(){
-//
-//   // add/remove classes for layout in issues
-//   // Get the elements to apply the classes
-//   // var article = document.querySelector('#canvas')
-//   var titleDiv = document.querySelector('#style-title');
-//   var innerDiv = document.querySelector('#row-style-title');
-//   var innerCol = document.querySelector('#col-style-title');
-//   var innerColAuthor = document.querySelector('#col-style-author');
-//
-//   // add the classes when the themed CSS file is applied
-//   function addClasses() {
-//     console.log(titleDiv);
-//     if (titleDiv !== null && innerDiv !== null && innerCol !== null && innerColAuthor !== null) {
-//
-//     titleDiv.classList.add('style-title', 'flex', 'container', 'align-items-center', 'justify-content-center');
-//     innerDiv.classList.add('style-title', 'flex', 'row', 'justify-content-center');
-//     innerCol.classList.add('style-title', 'col', 'col-md-6', 'col-lg-6', 'col-6');
-//     innerColAuthor.classList.add('style-title', 'flex', 'col', 'col-md-1', 'col-lg-1', 'col-1');
-//   }
-// }
-//   //remove the classes when theme file is not applied
-//   function removeClasses() {
-//     if (titleDiv !== null && innerDiv !== null && innerCol !== null && innerColAuthor !== null) {
-//
-//
-//     titleDiv.classList.remove('style-title', 'flex', 'container', 'align-items-center', 'justify-content-center');
-//     innerDiv.classList.remove('style-title', 'flex', 'row', 'justify-content-center');
-//     innerCol.classList.remove('style-title', 'col', 'col-md-6', 'col-lg-6', 'col-6');
-//     innerColAuthor.classList.remove('style-title', 'flex', 'col', 'col-md-1', 'col-lg-1', 'col-1');
-//
-//   }
-// }
-// //
-// // if (sessionStorage.getItem('href') === 'assets/css/bauhaus.css') {
-// //   addClasses();
-// // } else {
-// //   removeClasses();
-// // }
-//
-// // console.log(newTheme);    // rest of the code ...
-// });
 
 
 
